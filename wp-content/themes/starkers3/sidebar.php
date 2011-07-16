60761c805862bda4ce95209ef7ee3a9235051cc5
Eric Wendelin <emwendelin@gmail.com>
2011-07-16 10:21:24 -0600

    Adding Google Plus and LinkedIn to Follow section

--- wp-content/themes/starkers3/sidebar.php
+++ wp-content/themes/starkers3/sidebar.php
@@ -1,34 +1,36 @@
 <aside id="sidebar">
-    <section id="subscribe-container" class="section">
-        <header><h3>Follow</h3></header>
-        <div>
-        <a id="rsslink" href="http://feeds2.feedburner.com/EricWendelin"><span id="numsubscribers">1309</span></a> 
-        <a id="twitterlink" href="http://twitter.com/eriwen"><span id="numtweeters">419</span></a>
-        </div>
-    </section>
-    <section id="projects-container" class="section">
-       <header><h3>Projects</h3></header>
-       <nav><ul>
-       <li><a href="https://github.com/eriwen/griffon-jython" title="Jython plugin for Griffon">Griffon Jython plugin</a></li>
-       <li><a href="http://github.com/eriwen/groovyrtm" title="Groovy API for Remember The Milk">GroovyRTM</a></li>
-       <li><a href="http://stacktracejs.org" title="Micro-library to get a Javascript stacktrace on all browsers">stacktrace.js</a></li>
-       <li><a href="http://github.com/eriwen/cheqlist-javafx" title="JavaFX desktop application for Remember The Milk">Cheqlist</a></li>
-       </ul></nav>
-    </section>
-    <section id="featured-container" class="section" role="navigation">
-       <header><h3>Featured</h3></header>
-       <ul><li><a href="http://eriwen.com/bash/effective-shorthand/">Effective bash shorthand</a></li><li><a href="http://eriwen.com/tools/wikify-yourself/">Every programmer should have a Tiddlywiki</a></li><li><a href="http://eriwen.com/productivity/find-is-a-beautiful-tool/">Find is a beautiful tool</a></li><li><a href="http://eriwen.com/books/best-algorithms-book/">Best algorithms book I ever read</a></li><li><a href="http://eriwen.com/tools/grep-is-a-beautiful-tool/">grep is a beautiful tool</a></li><li><a href="http://eriwen.com/javascript/js-stack-trace/">Javascript stacktrace in any browser</a></li><li><a href="http://eriwen.com/javascript/highlight-search-results-with-js/">Highlight search results with JavaScript and CSS</a></li></ul>
-    </section>
-    <section id="elsewheres-container" class="section">
-        <header><h3>Elsewheres</h3></header>
+	<section id="subscribe-container" class="section">
+		<header><h3>Follow</h3></header>
+		<div>
+		<a id="rsslink" href="http://feeds2.feedburner.com/EricWendelin"></a> 
+		<a id="twitterlink" href="http://twitter.com/eriwen"></a>
+		<a id="gpluslink" href="https://plus.google.com/101550040936373881930"></a>
+		<a id="linkedinlink" href="http://www.linkedin.com/in/eriwen"></a>
+		</div>
+	</section>
+	<section id="projects-container" class="section">
+		<header><h3>Projects</h3></header>
+		<nav><ul>
+		<li><a href="https://github.com/eriwen/griffon-jython" title="Jython plugin for Griffon">Griffon Jython plugin</a></li>
+		<li><a href="http://github.com/eriwen/groovyrtm" title="Groovy API for Remember The Milk">GroovyRTM</a></li>
+		<li><a href="http://stacktracejs.org" title="Micro-library to get a Javascript stacktrace on all browsers">stacktrace.js</a></li>
+		<li><a href="http://github.com/eriwen/cheqlist-javafx" title="JavaFX desktop application for Remember The Milk">Cheqlist</a></li>
+		</ul></nav>
+	</section>
+	<section id="featured-container" class="section" role="navigation">
+		<header><h3>Featured</h3></header>
+		<ul><li><a href="http://eriwen.com/bash/effective-shorthand/">Effective bash shorthand</a></li><li><a href="http://eriwen.com/tools/wikify-yourself/">Every programmer should have a Tiddlywiki</a></li><li><a href="http://eriwen.com/productivity/find-is-a-beautiful-tool/">Find is a beautiful tool</a></li><li><a href="http://eriwen.com/books/best-algorithms-book/">Best algorithms book I ever read</a></li><li><a href="http://eriwen.com/tools/grep-is-a-beautiful-tool/">grep is a beautiful tool</a></li><li><a href="http://eriwen.com/javascript/js-stack-trace/">Javascript stacktrace in any browser</a></li><li><a href="http://eriwen.com/javascript/highlight-search-results-with-js/">Highlight search results with JavaScript and CSS</a></li></ul>
+	</section>
+	<section id="elsewheres-container" class="section">
+		<header><h3>Elsewheres</h3></header>
 <ul><li><a href="http://eriwen.com/contact/" title="Contact Eric">Contact Me</a></li>
 <li><a href="http://careers.stackoverflow.com/eriwen" title="Eric Wendelin's resume">Resume</a></li>
 <li><a href="http://gist.github.com/eriwen" title="Gists">Code Snippets</a></li>
 <li><a href="http://www.slideshare.net/emwendelin/" title="Slideshare presentations">Presentations</a></li>
 <li><a href="http://faceoffshow.com/2010/09/14/episode-86-hadoop/" title="Faceoff Show Hadoop podcast">Hadoop podcast</a></li>
 <li><a href="http://faceoffshow.com/2009/05/12/episode-16-green-to-the-selenium-scene/" title="Faceoff show Selenium podcast">Selenium podcast</a></li></ul>
-    </section>
-    <section id="promo" class="section">
+	</section>
+	<section id="promo" class="section">
 <a href='https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array' title='JS Array .sort'><div id='promotejs'></div></a>
-    </section>
+	</section>
 </aside>
