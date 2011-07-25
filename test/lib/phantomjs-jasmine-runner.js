1ec0754446e05333362a1e45be48b8ac1347f270
Eric Wendelin <emwendelin@gmail.com>
2011-07-25 15:03:44 -0600

    Adding PhantomJS runner

---
+++ test/lib/phantomjs-jasmine-runner.js
@@ -0,0 +1,47 @@
+phantom.injectJs("waitFor.js");
+
+if (phantom.args.length !== 1) {
+    console.log("Usage: phantomjs-jasmine-runner.js HTML_RUNNER");
+    phantom.exit();
+} else {
+    var htmlrunner = phantom.args[0],
+        page = new WebPage();
+    
+    // Swallow test output so we don't mess up JUnit XML
+    page.onConsoleMessage = function(msg, source, linenumber) {};
+
+    page.open(htmlrunner, function(status) {
+        if (status === "success") {
+            utils.core.waitfor(function() { // wait for this to be true
+                return page.evaluate(function() {
+                    return typeof(jasmine.phantomjsXMLReporterPassed) !== "undefined";
+                });
+            }, function() { // once done...
+                // Retrieve the result of the tests
+                var suitesResults = page.evaluate(function(){
+                    return jasmine.phantomjsXMLReporterResults;
+                });
+                
+                // Save the result of the tests in files
+                for (var i = 0, len = suitesResults.length; i < len; ++i) {
+                    try {
+                        console.log(suitesResults[i]["xmlbody"]);
+                    } catch (e) {
+                        console.log(e);
+                        console.log("phantomjs> Unable to save result of Suite '"+ suitesResults[i]["xmlfilename"] +"'");
+                    }
+                }
+                
+                // Return the correct exit status. '0' only if all the tests passed
+                phantom.exit(page.evaluate(function(){
+                    return jasmine.phantomjsXMLReporterPassed ? 0 : 1; //< exit(0) is success, exit(1) is failure
+                }));
+            }, function() { // or, once it timesout...
+                phantom.exit(1);
+            });
+        } else {
+            console.log("phantomjs> Could not load '" + htmlrunner + "'.");
+            phantom.exit(1);
+        }
+    });
+}
